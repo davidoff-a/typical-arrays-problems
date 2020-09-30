e187630ea33550abf112f713c9827dd2e20eae70
davidoff-a <davidoff.aleksei@gmail.com>
2020-09-30 23:16:10 +0300

    feat: add max, min,avg function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
+exports.avg = avg;
+exports.max = max;
+exports.min = min;
 
-exports.min = function min (array) {
-  return 0;
+function avg(array = []) { 
+  const length = array.length;
+  return length === 0
+    ? 0 
+    : array.reduce((sum, num) => sum + num, 0) / length;
 }
 
-exports.max = function max (array) {
-  return 0;
+function max(array) {
+  return getFirst(array, (a, b) => b - a);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+function min(array) {
+  return getFirst(array, (a, b) => a - b)
 }
+
+function getFirst(array = [], sortRule) {
+  const [first = 0, ...rest] = array.sort(sortRule);
+  return first;
+}
+// exports.min = function min (array=[]) {
+//   const arr = array.sort(a,b=>b-a);
+//   return arr[0]||0;
+// }
+
+// exports.max = function max (array=[]) {
+//   const arr = array.sort(a,b=>a-b);
+//   return arr[0]||0;
+// }
+
+// exports.avg = function avg (array=[]) {
+//   return 0;
+// }
+
